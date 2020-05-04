10edd94f772cf04c6a08c71d2d2601673ced3690
Nicola Bottari <nicolabottari@me.com>
2020-05-04 18:14:25 +0200

    Added more forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find location.", undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". Temperature: " + body.current.temperature + "C")
+            callback(undefined, body.current.weather_descriptions[0] + ". Temperature: " + body.current.temperature + "C. The humidity is " + body.current.humidity + "%. The local time is " + body.location.localtime + ".")
         }
     })
 
